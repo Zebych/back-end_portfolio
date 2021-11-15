47e4f547d48ca3bf1e1fa33019989eea17eaa0b9
Zebych <81613417+Zebych@users.noreply.github.com>
2021-11-15 17:04:47 +0300

    PORT

--- index.js
+++ index.js
@@ -13,14 +13,16 @@ let smtp_login = process.env.SMTP_LOGIN || "___";
 let smtp_password = process.env.SMTP_PASSWORD || "___";
 
 // create reusable transporter object using the default SMTP transport
+
 let transporter = nodemailer.createTransport({
     service: 'gmail',
- /*   port: 25,//587,
+    port: 25,//587,
     secure: false, // true for 465, false for other ports
     tls: {
         rejectUnauthorized: false
-    },*/
-    auth: {
+    },
+
+     auth: {
         user: smtp_login, // generated ethereal user
         pass: smtp_password, // generated ethereal password
     },
@@ -29,20 +31,19 @@ app.get('/', function (req, res) {
     res.send("HELLO");
 })
 app.post('/send', async function (req, res) {
-    let {message, contacts, name} = req.body
+    let {name, email, message} = req.body
     // send mail with defined transport object
     let info = await transporter.sendMail({
         from: "My profile page", // sender address
         to: "sanechek_1987@mail.ru", // list of receivers
         subject: "тестирую", // Subject line
-        // text: "Hello world?", // plain text body
         html: `<b>сообщение с моего portfolio</b>
 <div>
 <div>
 name:${name}
 </div>
 <div>
-contacts: ${contacts}
+contacts: ${email}
 </div>
 <div>
 message:${message}
