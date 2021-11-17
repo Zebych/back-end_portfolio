65580f2e70e549990bf4903207289817c3714c42
Zebych <81613417+Zebych@users.noreply.github.com>
2021-11-17 20:47:20 +0300

    working version on heroku

--- index.js
+++ index.js
@@ -7,38 +7,35 @@ app.use(cors({"origin": "*"}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-/*let smtp_login = process.env.SMTP_LOGIN
-let smtp_password = process.env.SMTP_PASSWORD*/
+let smtp_login = process.env.SMTP_LOGIN
+let smtp_password = process.env.SMTP_PASSWORD
 
 let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     secure: true, // true for 465, false for other ports
-    port: 465,//465,
+    port: 465,
     tls: {
         rejectUnauthorized: true
     },
     auth: {
-        user: "sanechek050187@gmail.com", // generated ethereal user
-        pass: "tardntes", // generated ethereal password
+        user: smtp_login, // generated ethereal user
+        pass: smtp_password, // generated ethereal password
 
     },
 });
-transporter.verify((err, success) => {
-    if (err) console.log(err);
-    console.log(("your config is correct"))
-})
+
 app.get('/', function (req, res) {
     res.send("HELLO");
 })
+
 app.post('/sendMessage', async function (req, res) {
-    try {
-        let {name, email, message} = req.body
-        // send mail with defined transport object
-        let info = await transporter.sendMail({
-            from: "My profile page", // sender address
-            to: "sanechek_1987@mail.ru", // list of receivers
-            subject: "тестирую", // Subject line
-            html: `<b>сообщение с моего portfolio</b>
+    let {name, email, message} = req.body
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: "My profile page", // sender address
+        to: "sanechek_1987@mail.ru", // list of receivers
+        subject: "тестирую", // Subject line
+        html: `<b>сообщение с моего portfolio</b>
 <div>
 <div>
 name: ${name}
@@ -50,15 +47,11 @@ email: ${email}
 message: ${message}
 </div>
 </div>`,
-        });
-    } catch (error) {
-        console.log(error)
-    }
-
+    });
     res.send("HELLO");
 });
+
 let port = process.env.PORT || 3010
-// let port =  3010
 app.listen(port, function () {
     console.log("Example")
 })
